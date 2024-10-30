1ac35dc65f8eb30797d26aaecd881b02dffebf0e
James Watson <james.watson-2@colorado.edu>
2024-10-30 14:40:28 -0600

    Data collection code, NEEDS TESTING AND ANALYSIS

--- zc_trace.py
+++ zc_trace.py
@@ -2,7 +2,7 @@
 ########## QUESTIONS TO ANSWER #####################################################################
 [?] Did the recording work?
 [?] How stable are the symbols? Do the memories they are derived from always change?
-[?] Do the readings that become symbols get erased after the action? Why?
+[?] Do the readings that become symbols get erased after the action? 
 [?] Do the symbol probabilities trend upward?
 [?] Do the symbol quality scores trend upward?
 [?] What is the impact of artifically boosting the quality of moved readings? Did it help?
@@ -25,6 +25,17 @@ set_blocks_env()
 set_experiment_env()
 
 
+########## HELPER FUNCTIONS ########################################################################
+
+def get_symbol_parents( objLst : list[GraspObj] ):
+    """ Filter out the objects that are parents of symbols """
+    rtnLst = list()
+    for obj in objLst:
+        if obj.SYM:
+            rtnLst.append( obj )
+    return rtnLst
+
+
 ########## ANALYSIS ################################################################################
 
 
@@ -37,18 +48,50 @@ for i, datum in enumerate( data ):
 
 
 """ [?] How stable are the symbols? Do the memories they are derived from always change? """
+""" [?] Do the symbol probabilities trend upward? """
+""" [?] Do the symbol quality scores trend upward? """
+""" [?] What is the impact of artifically boosting the quality of moved readings? Did it help? """
+# FIXME: PLOTS FOR ABOVE QUESTIONS
 
 symbolStability = list()
+combosPerAction = list()
+
 for i, datum in enumerate( data ):
     if datum['msg'] == 'symbols':
-        sym = dict()
-        for pair in datum['data']:
+
+        sym = { 't': datum['t'] }
+        for pair in datum['data']['pairs']:
             sym[ pair['symbol'].label ] = { 
+                'prob'   : pair['symbol'].prob,
+                'score'  : pair['symbol'].score,
                 'pose'   : extract_pose_as_homog( pair['symbol'] ),
                 'parIdx' : pair['parent'].index,
                 'parS'   : pair['parent'].score,
             }
         symbolStability.append( sym )
 
+        combosPerAction.append({ 
+            't'      : datum['t'],
+            'combos' : datum['data']['combos'],
+        })
+
+
 
 """ [?] Do the readings that become symbols get erased after the action? Why? """
+# FIXME: ANALYSIS FOR ABOVE QUESTION
+symbolDeletions = list()
+for i, datum in enumerate( data ):
+    symDel = { 't': datum['t'] }
+    added  = False
+    if datum['msg'] == 'ranking':
+        symDel['rnkRct'] = datum['data']["remRec"]
+        symDel['rnkCut'] = datum['data']["remCut"]
+        added = True
+    if datum['msg'] == 'memory':
+        symDel['LKGrec'] = datum['data']["LKGrec"]
+        symDel['LKGcut'] = datum['data']["LKGcut"]
+        added = True
+    if added:
+        symbolDeletions.append( symDel )
+
+
