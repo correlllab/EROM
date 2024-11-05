01242c0ac5c6683c8362c529c61fb402a39d5d4b
James Watson <james.watson-2@colorado.edu>
2024-11-05 15:57:01 -0700

    Draw scan

--- za_trace.py
+++ za_trace.py
@@ -17,9 +17,9 @@ from aspire.symbols import ObjPose, GraspObj, extract_pose_as_homog
 from aspire.BlocksTask import set_blocks_env
 
 from TaskPlanner import set_experiment_env
-from draw_beliefs import set_render_env
+from draw_beliefs import set_render_env, render_memory_list, render_scan_list
 
-path = "/home/james/EROM/data/EROM-Memories_10-30-2024_16-51-47.pkl"
+path = "/home/james/EROM/data/EROM-Memories_11-05-2024_13-43-39.pkl"
 data = list()
 with open( path, 'rb' ) as f:
     data = pickle.load( f )
@@ -57,57 +57,21 @@ for i, datum in enumerate( data ):
 """ [?] What is the impact of artifically boosting the quality of moved readings? Did it help? """
 # FIXME: PLOTS FOR ABOVE QUESTIONS
 
-symbolStability = list()
-combosPerAction = list()
 
-for i, datum in enumerate( data ):
-    if datum['msg'] == 'symbols':
-
-        sym = { 't': datum['t'] }
-        for pair in datum['data']['pairs']:
-            if pair['symbol'] is not None:
-                sym[ pair['symbol'].label ] = { 
-                    'prob'   : pair['symbol'].prob,
-                    'score'  : pair['symbol'].score,
-                    'pose'   : extract_pose_as_homog( pair['symbol'] ),
-                    'parIdx' : pair['parent'].index,
-                    'parS'   : pair['parent'].score,
-                }
-        symbolStability.append( sym )
-
-        combosPerAction.append({ 
-            't'      : datum['t'],
-            'combos' : datum['data']['combos'],
-        })
-
-
-
-""" [?] Do the readings that become symbols get erased after the action? Why? """
-# FIXME: ANALYSIS FOR ABOVE QUESTION
-symbolDeletions = list()
-for i, datum in enumerate( data ):
-    symDel = { 't': datum['t'] }
-    added  = False
-    if datum['msg'] == 'ranking':
-        symDel['rnkRct'] = datum['data']["remRec"]
-        symDel['rnkCut'] = datum['data']["remCut"]
-        added = True
-    if datum['msg'] == 'memory':
-        symDel['LKGrec'] = datum['data']["LKGrec"]
-        symDel['LKGcut'] = datum['data']["LKGcut"]
-        added = True
-    if added:
-        symbolDeletions.append( symDel )
 
 
 
 ########## DRAW SCANS ##############################################################################
 
+totMem = list()
+
 for i, datum in enumerate( data ):
     if datum['msg'] == 'memory':
-        # pprint( datum['data']['observation'] )
-        # pprint( datum['data']['scan'] )
-        # pprint( datum['data']['scan'] )
-        pprint( datum['data']['LKGrec'] )
+        # pprint(  )
+        totMem.extend( datum['data']['scan'] )
+        # render_memory_list( datum['data']['scan'] )
+
+render_scan_list( totMem )
+
 
 os.system( 'kill %d' % os.getpid() ) 
