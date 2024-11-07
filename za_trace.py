00f82b1c8304a1f9687979770d6229d0b9c924d6
William <williamxie00@gmail.com>
2024-11-07 16:52:40 -0700

    Segmentation seems to work!

--- za_trace.py
+++ za_trace.py
@@ -24,7 +24,11 @@ from draw_beliefs import ( set_render_env, render_memory_list, render_scan_list,
 
 # path = "/home/james/EROM/data/EROM-Memories_11-05-2024_13-43-39.pkl"
 # path = "/home/james/EROM/data/EROM-Memories_11-06-2024_18-09-37.pkl"
-path = "/home/will/james/erom/data/EROM-Memories_11-07-2024_16-10-32.pkl"
+# path = "/home/will/james/erom/data/EROM-Memories_11-07-2024_16-10-32.pkl"
+# path = "/home/will/james/erom/data/EROM-Memories_11-07-2024_16-24-41.pkl"
+# path = "/home/will/james/erom/data/EROM-Memories_11-07-2024_16-29-50.pkl"
+# path = "/home/will/james/erom/data/EROM-Memories_11-07-2024_16-36-58.pkl"
+path = "/home/will/james/erom/data/EROM-Memories_11-07-2024_16-42-59.pkl"
 data = list()
 with open( path, 'rb' ) as f:
     data = pickle.load( f )
@@ -83,7 +87,7 @@ for i, datum in enumerate( data ):
         readings = datum['data']['scan']
         for rdg in readings:
             rdg.cpcd.transform( camPose ) 
-            rdg.pose = ObjPose( np.dot( camPose, extract_pose_as_homog( rdg.pose ) ) )
+            # rdg.pose = ObjPose( np.dot( camPose, extract_pose_as_homog( rdg.pose ) ) )
         totMem.extend( readings )
         # break
 
