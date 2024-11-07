6fa80a18e153827fda181ef491d55d7ed32049fd
William <williamxie00@gmail.com>
2024-11-07 16:13:17 -0700

    Recording OWLv2 w/o errors

--- OWLv2_Segment.py
+++ OWLv2_Segment.py
@@ -75,23 +75,23 @@ def give_0():
 def bb_intersection_over_union( boxA, boxB ):
     """ Return IoU """
     # Author: Adrian Rosebrock, https://pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
-	# determine the (x, y)-coordinates of the intersection rectangle
-	xA = max(boxA[0], boxB[0])
-	yA = max(boxA[1], boxB[1])
-	xB = min(boxA[2], boxB[2])
-	yB = min(boxA[3], boxB[3])
-	# compute the area of intersection rectangle
-	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
-	# compute the area of both the prediction and ground-truth
-	# rectangles
-	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-	# compute the intersection over union by taking the intersection
-	# area and dividing it by the sum of prediction + ground-truth
-	# areas - the interesection area
-	iou = interArea / float(boxAArea + boxBArea - interArea)
-	# return the intersection over union value
-	return iou
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    # return the intersection over union value
+    return iou
 
 
 
@@ -258,8 +258,10 @@ class Perception_OWLv2:
                     cpcd = None
 
                     try:
+                        # print( [hit_i['bbox'],] )
                         _, cpcd, _, _ = pcd.get_segment(
-                            [hit_i['bbox'],],
+                            # [hit_i['bbox'],],
+                            [(hit_i['bbox'],0,),],
                             0,
                             result['rgbd'], # Just use the last image in the series
                             self.rsc,
@@ -279,7 +281,7 @@ class Perception_OWLv2:
                         'Probability': defaultdict( give_0 ),
                         'Count'      : 1,
                         ## Frozen ##
-                        'bbox'       : hit_i['bbox']
+                        'bbox'       : hit_i['bbox'],
                         'Pose'       : self.get_pcd_pose( cpcd ),
                         'Time'       : now(),
                         'CPCD'       : { 'points' : np.asarray( cpcd.points ).copy(),
