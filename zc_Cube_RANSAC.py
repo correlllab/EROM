77012d366fd7721758bc28920a8e9e0beea41a59
James Watson <james.watson-2@colorado.edu>
2024-11-18 15:54:00 -0700

    This was not urgent, PLEASE STOP

--- zc_Cube_RANSAC.py
+++ zc_Cube_RANSAC.py
@@ -64,6 +64,7 @@ def get_cube( sideLen, homog ):
 def get_normals_diameters_and_centers( vertTriples : np.ndarray ):
     """ Return the normal (and "diameter") at each vertex, assuming every three vertices form a triangle """
     # NOTE: Diameters will be used a distance heuristic
+    # NOTE: This function outputs some distances 3 times in order to make the Numpy operations nice
     rtnNrm = np.ones( vertTriples.shape )
     rtnCen = np.ones( vertTriples.shape )
     rtnDia = np.ones( len( vertTriples ) )
@@ -83,30 +84,54 @@ def get_normals_diameters_and_centers( vertTriples : np.ndarray ):
     return rtnNrm, rtnDia, rtnCen
 
 
-def min_dist_to_mesh( q, verts ):
-    """ Given a list of triangle verts, Return the least distance to the mesh, HACK: We don't actually care if it's very accurate! """
+def min_dist_to_mesh( q, verts, norms, diams, cntrs ):
+    """ Given a list of triangle verts, Return the least distance from `q` to the mesh, HACK: We don't actually care if it's very accurate! """
     # HACK: This function uses a distance heuristic to determine if the point distance or plane distance is correct
     # HACK: This function does NOT take into account the distance to the triangle edge in the case that it is the least distance
+    # NOTE: This function computes some distances 3 times in order to make the Numpy operations nice
     factr = 1.5 # Bubble factor
     Npnts = len( verts )
-    norms, diams, cntrs = get_normals_diameters_and_centers( verts )
     diffs = np.subtract( verts, q )
-    cenDs = np.subtract( cntrs, q )
-    # NOTE: This computes the distance to each plane 3 times
+    cenDf = np.subtract( cntrs, q )
+    cenDs = np.linalg.norm( cenDf, axis = 1 )
     plnDs = np.sum( norms * diffs, axis = 1, keepdims = True ) # https://stackoverflow.com/q/62500584
     pntDs = np.linalg.norm( diffs, axis = 1 ) # https://stackoverflow.com/a/7741976
-    # HACK: CHOOSE POINT DISTANCE IF OUTSIDE OF BUBBLE, CHOOSE MIN IF INSIDE BUBBLE
-    dMin = 1e9
+    dMin  = 1e9
     for i in range( Npnts ):
+        # HACK: CHOOSE POINT DISTANCE IF OUTSIDE OF BUBBLE, CHOOSE PLANE DISTANCE IF INSIDE BUBBLE
         if (cenDs[i,0] > (diams[i]*factr)):
             dMin = min( dMin, pntDs[i,0] )
         else:
-            dMin = min( dMin, min( plnDs[i,0], pntDs[i,0] ) )
+            dMin = min( dMin, plnDs[i,0] )
     return dMin
-    
 
-# FIXME, START HERE: TEST A FEW POINTS WITH A UNIT CUBE AT THE ORIGIN (DRAW IT?)
-# FIXME: IMPLEMENT RANSAC
-# FIXME: TEST RANSAC WITH A NOISY CUBE OF POINTS
-# FIXME: GENERATE A PARTIAL PCD AS FROM A MASKED OBLIQUE VIEW
-# FIXME: TEST RANSAC WITH A MASKED OBLIQUE VIEW
+
+def approx_cube_RANSAC( points, sideLen, seed = None ):
+    """ Fit the pose of a cube of size `sideLen` to `points`, using a slightly inaccurate distance model """
+    Npnts = len( points )
+    cPose = np.eye(4)
+    dPnts = np.zeros( Npnts )
+    if seed is not None:
+        cPose = seed
+    else:
+        cPose[0:3,3] = np.mean( points, axis = 0 )
+
+    ##### FIXME: IMPLEMENT RANSAC #####
+    
+    ## Get model at pose ##
+    verts = get_cube( sideLen, cPose )
+    norms, diams, cntrs = get_normals_diameters_and_centers( verts )
+    
+    ## Calc distances to model ##
+    for i, q in enumerate( points ):
+        dPnts[i] = min_dist_to_mesh( q, verts, norms, diams, cntrs )
+
+
+
+if __name__ == "__main__":
+    # FIXME, START HERE: TEST A FEW POINTS WITH A UNIT CUBE AT THE ORIGIN (DRAW IT?)
+    pass
+
+    # FIXME: TEST RANSAC WITH A NOISY CUBE OF POINTS
+    # FIXME: GENERATE A PARTIAL PCD AS FROM A MASKED OBLIQUE VIEW
+    # FIXME: TEST RANSAC WITH A MASKED OBLIQUE VIEW
