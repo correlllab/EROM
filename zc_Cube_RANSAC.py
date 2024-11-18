3ceddac6989f960dee8aefa78a1c22d37f14dc04
James Watson <james.watson-2@colorado.edu>
2024-11-18 15:25:44 -0700

    START HERE: TEST A FEW POINTS WITH A UNIT CUBE AT THE ORIGIN (DRAW IT?)

--- zc_Cube_RANSAC.py
+++ zc_Cube_RANSAC.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from aspire.homog_utils import vec_unit
+
 def get_cube( sideLen, homog ):
     """ Return a 3x36 array of cube vertices, transformed by `homog` """
     hl = sideLen/2.0
@@ -59,26 +61,52 @@ def get_cube( sideLen, homog ):
     return np.dot( homog, vrts.transpose() )[:3,:].transpose()
 
 
-def get_normals( vertTriples : np.ndarray ):
-    """ Return the normal at each vertex, assuming every three vertices form a triangle """
+def get_normals_diameters_and_centers( vertTriples : np.ndarray ):
+    """ Return the normal (and "diameter") at each vertex, assuming every three vertices form a triangle """
+    # NOTE: Diameters will be used a distance heuristic
     rtnNrm = np.ones( vertTriples.shape )
+    rtnCen = np.ones( vertTriples.shape )
+    rtnDia = np.ones( len( vertTriples ) )
     for i in range( 0, len( vertTriples ), 3 ):
         p0 = vertTriples[i  ,:3]
         p1 = vertTriples[i+1,:3]
         p2 = vertTriples[i+2,:3]
         v0 = np.subtract( p1, p0 )
         v1 = np.subtract( p2, p1 )
-        ni = np.cross( v0, v1 )
+        v2 = np.subtract( p0, p2 )
+        mx = np.max( [np.linalg.norm( v0 ), np.linalg.norm( v1 ), np.linalg.norm( v2 ),] )
+        ni = vec_unit( np.cross( v0, v1 ) )
+        ci = ( p0 + p1 + p2 ) / 3.0
         rtnNrm[ i:i+3, :3 ] = [ni, ni, ni,]
-    return rtnNrm
+        rtnCen[ i:i+3, :3 ] = [ci, ci, ci,]
+        rtnDia[ i:i+3 ] = [mx, mx, mx,]
+    return rtnNrm, rtnDia, rtnCen
 
 
 def min_dist_to_mesh( q, verts ):
-    """ Given a list of triangle verts, Return the least distance to the mesh """
+    """ Given a list of triangle verts, Return the least distance to the mesh, HACK: We don't actually care if it's very accurate! """
+    # HACK: This function uses a distance heuristic to determine if the point distance or plane distance is correct
     # HACK: This function does NOT take into account the distance to the triangle edge in the case that it is the least distance
-    norms = get_normals( verts )
+    factr = 1.5 # Bubble factor
+    Npnts = len( verts )
+    norms, diams, cntrs = get_normals_diameters_and_centers( verts )
     diffs = np.subtract( verts, q )
+    cenDs = np.subtract( cntrs, q )
     # NOTE: This computes the distance to each plane 3 times
     plnDs = np.sum( norms * diffs, axis = 1, keepdims = True ) # https://stackoverflow.com/q/62500584
     pntDs = np.linalg.norm( diffs, axis = 1 ) # https://stackoverflow.com/a/7741976
-    return min( np.min( plnDs ), np.min( pntDs ) )
+    # HACK: CHOOSE POINT DISTANCE IF OUTSIDE OF BUBBLE, CHOOSE MIN IF INSIDE BUBBLE
+    dMin = 1e9
+    for i in range( Npnts ):
+        if (cenDs[i,0] > (diams[i]*factr)):
+            dMin = min( dMin, pntDs[i,0] )
+        else:
+            dMin = min( dMin, min( plnDs[i,0], pntDs[i,0] ) )
+    return dMin
+    
+
+# FIXME, START HERE: TEST A FEW POINTS WITH A UNIT CUBE AT THE ORIGIN (DRAW IT?)
+# FIXME: IMPLEMENT RANSAC
+# FIXME: TEST RANSAC WITH A NOISY CUBE OF POINTS
+# FIXME: GENERATE A PARTIAL PCD AS FROM A MASKED OBLIQUE VIEW
+# FIXME: TEST RANSAC WITH A MASKED OBLIQUE VIEW
