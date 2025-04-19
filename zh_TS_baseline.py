c65dfb2208290a2daa87594863906947b6749297
James Watson <james.watson-2@colorado.edu>
2025-04-18 18:14:11 -0600

    Need to compare closest to what was recorded

--- zh_TS_baseline.py
+++ zh_TS_baseline.py
@@ -4,6 +4,7 @@ from collections import deque
 
 import numpy as np
 
+from aspire.env_config import env_var
 from aspire.symbols import GraspObj, euclidean_distance_between_symbols
 from aspire.BlocksTask import set_blocks_env
 
@@ -29,6 +30,7 @@ set_render_env()
 
 ########## ANALYSIS ################################################################################
 _TS_DETERM = True
+_TS_DETAIL = True
 
 """
 * `[ ]` What if a reading overlaps with more than one reading: Which should it contribute to?
@@ -36,65 +38,102 @@ _TS_DETERM = True
 
 """
 
-pklPath = pkls[0]
-
 if _TS_DETERM:
-    print( f"Loading {pklPath} ...\n" )
-    with open( pklPath, 'rb' ) as f:
-        data = pickle.load( f )
-        for datum in data:
-            print()
-            tMsg  = datum['msg']
-            tData = datum['data']
-            print( f"{datum['t']} : {tMsg}" )
 
+    mxMty = 0.0
+    mxIdx = -1
 
-            if tMsg == "ObsMeta":
-                # print( list( tData.keys() ) ) # `dict`
+    # for pklDex, pklPath in enumerate( pkls ):
+    for pklDex, pklPath in enumerate( pkls[3:4] ):
 
-                inpt = tData['input']
-                print( f"input: {list(inpt.keys())}" ) # `dict`
-                for k, v in inpt.items(): # ['query', 'abbrv', 'image', 'depth', 't']
-                    print( f"\t{list(v.keys())}" )
-                    
-                hits = tData['hits']
-                print( f"hits: {type(hits)}" ) # `list`
-                print( f"\t{list(hits[0].keys())}" ) # ['bbox', 'bboxi', 'score', 'label', 'image', 'query', 'abbrv', 'shotID']
+        totRun = 0.0 
+        totMty = 0.0
 
-                print()
+        print( f"Loading {pklPath} ...\n" )
 
-
-            elif tMsg == "memory":
-                # print( list( tData.keys() ) ) # `dict`
-
-                scan = tData['scan']
-                print( f"scan: {type(scan)}" ) # `list`
-                print( f"\t{scan[0]}" ) # `GraspObj`
-                Mdst = np.zeros( (len(scan),len(scan),) )
-                for i, obj_i in enumerate( scan ):
-                    for j, obj_j in enumerate( scan ):
-                        if i != j:
-                            Mdst[i,j] = euclidean_distance_between_symbols( obj_i, obj_j )
-                
-                blfs = tData['beliefs']
-                print( f"beliefs: {type(blfs)}" ) # `list`
-                print( f"\t{blfs[0]}" ) # `GraspObj`
-                Ndst = np.zeros( (len(blfs),len(scan),) )
-                for i, obj_i in enumerate( blfs ):
-                    for j, obj_j in enumerate( scan ):
-                        Ndst[i,j] = euclidean_distance_between_symbols( obj_i, obj_j )
-
-                print( Ndst )
-                
-                print()
-
-
-            elif tMsg == "symbols":
-                print( type( tData ) ) # `list``
-                print()
+        with open( pklPath, 'rb' ) as f:
             
+            data   = pickle.load( f )
+            totRun += (data[-1]['t'] - data[0]['t'])
+            tLst   = data[0]['t']
+
+            for datum in data:
+                tMsg  = datum['msg']
+                tData = datum['data']
+                if _TS_DETAIL:
+                    print( f"\n{datum['t']} : {tMsg}" )
 
 
+                if tMsg == "ObsMeta":
+                    # print( list( tData.keys() ) ) # `dict`
+
+                    inpt = tData['input']
+                    if _TS_DETAIL:
+                        print( f"input: {list(inpt.keys())}" ) # `dict`
+                        for k, v in inpt.items(): # ['query', 'abbrv', 'image', 'depth', 't']
+                            print( f"\t{list(v.keys())}" )
+                        
+                    hits = tData['hits']
+                    if _TS_DETAIL:
+                        print( f"hits: {type(hits)}" ) # `list`
+                        print( f"\t{list(hits[0].keys())}" ) # ['bbox', 'bboxi', 'score', 'label', 'image', 'query', 'abbrv', 'shotID']
+                        print()
+
+
+                elif tMsg == "memory":
+
+                    scan = tData['scan']
+                    if _TS_DETAIL:
+                        print( f"scan: {type(scan)}" ) # `list`
+                        print( f"\t{scan[0]}" ) # `GraspObj`
+                    Mdst = np.zeros( (len(scan),len(scan),) )
+                    for i, obj_i in enumerate( scan ):
+                        for j, obj_j in enumerate( scan ):
+                            if i != j:
+                                Mdst[i,j] = euclidean_distance_between_symbols( obj_i, obj_j )
+                    Mcls = np.where( Mdst < env_var("_BAYES_RAD_L2_M"), 1, 0)
+                    
+                    blfs = tData['beliefs']
+                    if _TS_DETAIL:
+                        print( f"beliefs: {type(blfs)}" ) # `list`
+                        print( f"\t{blfs[0]}" ) # `GraspObj`
+                    Ndst = np.zeros( (len(blfs),len(scan),), dtype = int )
+                    for i, obj_i in enumerate( blfs ):
+                        for j, obj_j in enumerate( scan ):
+                            Ndst[i,j] = euclidean_distance_between_symbols( obj_i, obj_j )
+                    Ncls = np.where( Ndst < env_var("_BAYES_RAD_L2_M"), 1, Ndst)
+
+                    if _TS_DETAIL:
+                        print( f"\nThere are {len(blfs)} beliefs!\n" )
+                        print( Mcls )
+                        print()
+                        # print( Ncls ) # This is always 1's!
+                        # print()
+
+
+                elif tMsg == "symbols":
+
+                    elapsed = datum['t'] - tLst
+                    tLst    = datum['t']
+                    Nsym    = len( tData )
+
+                    if Nsym:
+                        print( f"There are {Nsym} symbols!" )
+                    else:
+                        totMty += elapsed
+
+                    print()
+
+        if totMty > mxMty:
+            mxMty = totMty
+            mxIdx = pklDex
+        mins, secs = divmod( totRun, 60.0 )
+        mins = int( mins )
+        print( f"\nSolver spent {totMty/totRun:.4f} of {mins}:{secs:.1f} without determinized symbols!" )  
+    
+    print( f"\nWorst run spent {mxMty} seconds without symbols!, Index: {mxIdx}" )
+
+# Solver spent 0.2068 of 353:12.0 without determinized symbols!
 
 ########## CLEAN-UP ################################################################################
 print( "\n\n" )
