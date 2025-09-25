f33238a90b6a18d3789e1ceeff0e4740b45e2c8b
James Watson <james.watson-2@colorado.edu>
2025-09-25 13:56:25 -0600

    All my grading shit is at HOOOOMMME

--- zn_Analytical.py
+++ zn_Analytical.py
@@ -1,9 +1,34 @@
+import os
+from collections import deque
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
 SIMPLIFCIATIONS:
 * Use mean times for {`mv`, `pl`, `rm`} 
 """
+
+_TITLE_FONT_SIZE = 13
+_TIGHT_MARGIN    =  0.05
+
+def make_line_plot( X, Y, plotTitle, fName = None, xLabel = 'Makespan', yLabel = 'Occurrences', savefig = True ):
+    """ Create Histogram """
+    if savefig:
+        plt.clf()
+    plt.margins( _TIGHT_MARGIN )
+    print( f"\n{plotTitle}" )
+    # plt.hist( series )
+    plt.plot( X, Y )
+    plt.title( plotTitle, fontsize = _TITLE_FONT_SIZE ) # Set the title && font size
+    plt.xlabel( xLabel ) # ---------------- Setting the x-axis label
+    plt.ylabel( yLabel ) # ---------------- Setting the y-axis label
+    plt.tight_layout()
+    if savefig:
+        plt.savefig( fName )
+        return plt.gca()
+    else:
+        plt.show()
 
 
 def m_An( m_mv, m_pl, m_rm, PofN_A ) -> float:
@@ -17,15 +42,57 @@ def m_i( i : int, m_mv : float, m_pl : float, PofN : list[float], m_rm : float )
     def alpha( i, m_mv, m_pl, PofN, m_rm ):
         return m_mv + m_pl + PofN[i] * m_rm
     
-    def beta( m_rm, m_ : list[float]  ):
-        if len( m_ ) > 1:
-            rtnBta = m_rm
-            for k in range( len( m_ ) ):
-                rtnBta += m_rm + m_[k]
-            return rtnBta
-        elif len( m_ ) == 1:
-            return m_rm + m_[0]
-        else:
-            raise ValueError( f"Got a list of length {len( m_ )}" )
+    def beta( i, m_rm, m_ : list[float] = None  ):
+        rtnBta = m_rm
+        for k in range( i-1 ):
+            rtnBta += m_rm + m_[k]
+        return rtnBta
+
+    m_i = [0.0,]
     
-    a = alpha( i, m_mv, m_pl, PofN, m_rm )
+    for ii in range( 1, i+1 ):
+        if ii == 1:
+             m_i.append( m_An( m_mv, m_pl, m_rm, PofN[1] ) )
+        else:
+            a  = alpha( ii, m_mv, m_pl, PofN, m_rm )
+            n2 = 0.0
+            for j in range( 1, ii-1 ):
+                n2 += PofN[j] * beta( j, m_rm, m_i )
+            dn = 1 - PofN[ii]
+            for j in range( 1, ii-1 ):
+                dn -= PofN[j]
+            m_i.append( (a + n2)/dn )
+
+    return m_i[-1]
+
+
+########## MAIN ####################################################################################
+if __name__ == "__main__":
+    tAct = 30.00
+    m_mv = tAct
+    m_pl = tAct
+    m_rm = tAct
+    # Pcas = np.linspace( 0.0, 0.999, num = 100, endpoint = True )
+    # Pcas = np.linspace( 0.0, 0.499, num = 100, endpoint = True )
+    # Pcas = np.linspace( 0.0, 0.400, num = 100, endpoint = True )
+    Pcas = np.linspace( 0.0, 0.450, num = 100, endpoint = True )
+    Ymak = deque()
+    for Pcnf in Pcas:
+        PofN = [None, Pcnf, Pcnf, Pcnf,]
+        m_A  = m_An( m_mv, m_pl, m_rm, PofN[1] )
+        m_1  = m_i( 1, m_mv, m_pl, PofN, m_rm )
+        m_3  = m_i( 3, m_mv, m_pl, PofN, m_rm )
+        print( f"1-Tower Makespan: {m_1} -vs- {m_A}" )
+        print( f"3-Tower Makespan: {m_3}\n" )
+        Ymak.append( m_3 )
+    make_line_plot( Pcas, Ymak, "Test Plot",
+                   xLabel = 'Confusion', yLabel = "Makespan [s]",
+                   savefig = False )
+
+
+########## EXIT ####################################################################################
+print( "\n\n" )
+os.system( 'kill %d' % os.getpid() ) 
+
+
+    
