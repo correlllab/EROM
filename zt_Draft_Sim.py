d687f0928dc448dcc0e47fa83447d2a1e9e6447a
James Watson <james.watson-2@colorado.edu>
2026-05-29 13:50:02 -0600

    Restart sim

---
+++ zt_Draft_Sim.py
@@ -0,0 +1,20 @@
+"""
+zt_Sim_Draft.py
+
+"""
+
+class Event_Recorder:
+    """ Record events and make it easy to take metrics """
+    pass
+
+
+class Dice_Engine:
+    """ Use the recorded metrics to generate outcomes """
+    pass
+
+
+class Robot_Planner:
+    """ Use the recorded metrics to simulate actions """
+    pass
+
+
