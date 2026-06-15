84e82b2b5266c15ef9d707e960de4ee1217edde3
James Watson <james.watson-2@colorado.edu>
2026-06-15 16:07:50 -0600

    Dizzy and not sure if I should continue

--- ya_SimClasses.py
+++ ya_SimClasses.py
@@ -9,9 +9,9 @@ class SimBlock:
     def __init__( self, label = None, pose = None, stacked = False , blocked = False ):
         """ Minimal state to represent a stacked block """
         SimBlock.count += 1
-        self.id   : int   = SimBlock.count
-        self.label: str   = label
-        self.pose : float = pose
+        self.id   : int   = SimBlock.count # "Unique" Identifier
+        self.label: str   = label # -------- Class of the block
+        self.pose : float = pose # --------- Pose as float: {Whole: Pose}.{Frac: Distance from Ideal [m]}
 
 
     def __repr__( self ):
@@ -22,6 +22,6 @@ class SimBlock:
     def copy( self ):
         """ Make a copy w a new ID """
         rtnObj = SimBlock()
-        rtnObj.label   = self.label   
-        rtnObj.pose    = self.pose    
+        rtnObj.label = self.label   
+        rtnObj.pose  = self.pose    
         return rtnObj
