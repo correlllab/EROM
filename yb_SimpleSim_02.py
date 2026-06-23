151391a7703b6f4ffc067a6439351100949a6661
James Watson <james.watson-2@colorado.edu>
2026-06-23 16:57:30 -0600

    `SimBlock`

--- yb_SimpleSim_02.py
+++ yb_SimpleSim_02.py
@@ -22,7 +22,7 @@ from aspire.BlocksTask import set_blocks_env
 ### Local ### 
 from env_config import set_experiment_env
 from analysis_Utils import DiceBinary_CDF, Loc, crash_out
-from ya_SimClasses import SimBlock
+
 
 
 """ ########## DEV_PLAN ############################################################################
@@ -72,6 +72,34 @@ from ya_SimClasses import SimBlock
 
 
 ########## SIMULATION CLASSES ######################################################################
+
+##### Simulated Block #####################################################
+
+class SimBlock:
+    """ Container class for a Block """
+    count = 0
+
+    def __init__( self, label = None, pose = None ):
+        """ Minimal state to represent a stacked block """
+        SimBlock.count += 1
+        self.id   : int   = SimBlock.count # "Unique" Identifier
+        self.label: str   = label # -------- Class of the block
+        self.pose : float = pose # --------- Pose as float: {Whole: Pose}.{Frac: Distance from Ideal [m]}
+
+
+    def __repr__( self ):
+        """ Print state """
+        return f"({self.label} @ {self.pose}), id: {self.id}"
+
+    
+    def copy( self ):
+        """ Make a copy w a new ID """
+        rtnObj = SimBlock()
+        rtnObj.label = self.label   
+        rtnObj.pose  = self.pose    
+        return rtnObj
+
+
 
 ##### Action ##############################################################
 
